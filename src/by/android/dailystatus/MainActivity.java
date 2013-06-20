f2cfce2e7794ef7f363e157b58b5a2d55ac0179e
Egor <sekt88@gmail.com>
2013-06-20 09:46:27 +0300

    update layots

--- src/by/android/dailystatus/MainActivity.java
+++ src/by/android/dailystatus/MainActivity.java
@@ -4,31 +4,32 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
-public class MainActivity extends SherlockListActivity {
+public class MainActivity extends SherlockActivity {
+
+	private static final String[] DATA = { "Sunday", "Monday", "Tuesday",
+			"Wednesday", "Thursday", "Friday", "Saturday" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		getSupportActionBar().setBackgroundDrawable(
 				new ColorDrawable(Color.parseColor("#0e78c9")));
-		
-	}
-	
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //Used to put dark icons on light action bar
 
-        menu.add("Save")
-            .setIcon(R.drawable.ic_compose)
-            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Used to put dark icons on light action bar
+
+		menu.add("Save").setIcon(R.drawable.ic_compose)
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 		return super.onCreateOptionsMenu(menu);
 	}
-
 
 }
