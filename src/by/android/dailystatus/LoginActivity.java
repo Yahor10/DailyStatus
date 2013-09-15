8d805c1cf4d981ae56484c8039b793439add3a11
Kucheryavaya <kucheryavayalyudmila@gmail.com>
2013-09-15 12:33:34 +0300

    add RegisterActivity change Main on Login as start activity
    Create RegisterActivity
    set fields into DB

--- src/by/android/dailystatus/LoginActivity.java
+++ src/by/android/dailystatus/LoginActivity.java
@@ -1,20 +1,35 @@
 package by.android.dailystatus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class LoginActivity extends Activity {
 
-	private Spinner mCountView;
+	//private Spinner mCountView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		setContentView(R.layout.login);
+		
+		Button btnCreateAccount = (Button) findViewById(R.id.btnCreateAccount);
+		btnCreateAccount.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
 
-		mCountView = (Spinner) findViewById(R.id.count);
+		/*mCountView = (Spinner) findViewById(R.id.count);
 		// Create an ArrayAdapter using the string array and a default spinner
 		// layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -23,7 +38,7 @@ public class LoginActivity extends Activity {
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
-		mCountView.setAdapter(adapter);
+		mCountView.setAdapter(adapter);*/
 	}
 
 }
---
+++ src/by/android/dailystatus/RegisterActivity.java
@@ -0,0 +1,79 @@
+package by.android.dailystatus;
+
+import by.android.dailystatus.orm.model.UserORM;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class RegisterActivity extends Activity  {
+
+	private String[] data = {"woman", "man"};
+	private String sex;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	private EditText etFirstName;
+	private EditText etLastName;
+	private EditText etEmail;
+	private EditText etPassword;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		etFirstName = (EditText) findViewById(R.id.etFirstName);
+		etLastName = (EditText) findViewById(R.id.etFirstName);
+		etEmail = (EditText) findViewById(R.id.etFirstName);
+		etPassword = (EditText) findViewById(R.id.etFirstName);
+		Spinner spinnerSex = (Spinner) findViewById(R.id.spinner);
+		Button btnOk = (Button) findViewById(R.id.btnOk);
+		
+		spinnerSex.setAdapter(adapter);
+		spinnerSex.setPrompt(getResources().getString(R.string.sex));
+		spinnerSex.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				sex = data[position];
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+			}
+		});
+		
+		btnOk.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				firstName = etFirstName.getText().toString();
+				lastName = etLastName.getText().toString();
+				email = etEmail.getText().toString();
+				password = etPassword.getText().toString();
+				//не знаю что присваивать полю id
+				//насколько я понимаю мы id должны автоматом генерить 
+				//в зависимости от того, какой последний у нас в базе, 
+				//т.е. id = (n+1), где n = последний id в БД
+				//но я не знаю где брать это значение
+				//может чего тут неправильно я делаю
+				int id = 0;
+				UserORM.insertUser(RegisterActivity.this, new UserORM(id, firstName, lastName, password, email));
+			}
+		});
+		
+		
+		
+	}
+
+}
+
